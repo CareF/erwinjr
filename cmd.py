aaf6f7ca2ccdaca57c652c1d2b2ad47b74e849b8
CareF <CareF.Lm@gmail.com>
2017-11-02 13:50:25 -0400

    cmd line comment update

--- cmd.py
+++ cmd.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python2
 # -*- coding:utf-8 -*-
+
+#============================================================================
+# ErwinJr is a simulation program for quantum semiconductor lasers.
+# Copyright (C) 2017 Ming Lyu (CareF)
+#
+# A portion of this code is Copyright (c) 2011, California Institute of 
+# Technology ("Caltech"). U.S. Government sponsorship acknowledged.
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#============================================================================
+
+# This is a command line performance test, and can be extended to a command
+# line tool for ErwinJr
+
 from __future__ import division
 import numpy as np
 from QCLayers import QCLayers
